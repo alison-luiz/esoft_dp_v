c21ccfc1e5f842d0a377532304a3c304169380fd
Alison Luiz <alison_luiz@outlook.com.br>
2024-05-22 20:32:25 -0300

    feat: added task with Map()

--- ProdutosMap/Map.js
+++ ProdutosMap/Map.js
@@ -11,14 +11,22 @@ const produtos = [
   { nome: "Batata", categoria: "Legumes" },
 ];
 
+// Constante que armazena um novo Map()
 const produtosMap = new Map();
 
+// Percorrer o array de produtos usando forEach - armazenando na variável "produto"
 produtos.forEach((produto) => {
+  // Um if que verifica se a categoria do produto não existe no Map()
   if (!produtosMap.has(produto.categoria)) {
+    // Se ela não existir, ele vai criar um novo array vazio para essa categoria no Map()
     produtosMap.set(produto.categoria, []);
   }
 
+  // Então é só adicionar o produto no array da categoria correspondente
+  // Ele usa .get() para pegar o array da categoria
+  // e .push() para adicionar o produto
   produtosMap.get(produto.categoria).push(produto);
 });
 
+// Printar o Map()
 console.log(produtosMap);
